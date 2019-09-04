52d1a97a9e824671ddbff6e1eddd6b484d308fee
Shane Harper <sharper@mosaiclearning.com>
2019-09-04 16:11:58 -0400

    add routes and views directories

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
+const path = require('path');
+
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-    console.log('this always runs');
-    next();
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/add-product', (req, res, next) => {
-    console.log('in another middleware bruh');
-    res.send('<h1>Hello from express!</h1>')
-});
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/', (req, res, next) => {
-    console.log('in another middleware');
-    res.send('<h1>Hello from express!</h1>')
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
