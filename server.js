daa339d6da4e8892461b18d02fd16c4cf0df52a6
Shane Harper <sharper@mosaiclearning.com>
2019-09-14 07:09:38 -0400

    add styles

--- server.js
+++ server.js
@@ -5,16 +5,18 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const rootDir = require('./util/path');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 app.listen(3000);
