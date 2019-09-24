9be34807bb678f9ed51717aeb32db4c349093172
Shane Harper <sharper@mosaiclearning.com>
2019-09-23 21:22:09 -0400

    add pug branch

--- routes/shop.js
+++ routes/shop.js
@@ -5,8 +5,10 @@ const express = require('express');
 const rootDir = require('../util/path');
 
 const router = express.Router();
+const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
+    console.log(adminData.products);
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
     //res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const rootDir = require('./util/path');
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
