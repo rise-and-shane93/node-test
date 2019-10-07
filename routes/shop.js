957ac583e87db63de5a1267b84a1dacbd9c7153b
Shane Harper <cdtharper@gmail.com>
2019-10-06 22:40:09 -0400

    converted other files to pug

--- routes/shop.js
+++ routes/shop.js
@@ -11,7 +11,7 @@ router.get('/', (req, res, next) => {
     // console.log(adminData.products);
     // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
     const {products} = adminData;
-    res.render('shop', {docTitle: 'Shop', prods: products});
+    res.render('shop', {pageTitle: 'Shop', prods: products, path: '/'});
 });
 
 module.exports = router;
